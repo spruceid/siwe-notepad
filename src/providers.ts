ec6ea185af40d50d44d7f80418e70a6c826e126a
Samuel Gbafa <skgbafa@gmail.com>
2022-11-15 17:01:25 -0800

    remove comments and add signout functions

--- src/providers.ts
+++ src/providers.ts
@@ -67,7 +67,7 @@ const signIn = async (connector: Providers) => {
         updateTitle(ens?.domain || address);
 
         const res = await fetch(`/api/me`);
-        if (res.status === 200) {  // this doesn't work yet without ssx-server
+        if (res.status === 200) {
             res.json().then(({ text, address, ens }) => {
                 connectedState(text, address, ens);
                 return;
@@ -85,10 +85,8 @@ const signIn = async (connector: Providers) => {
 const signOut = async () => {
     updateTitle('Untitled');
     updateNotepad('');
-    return fetch('/api/sign_out', {
-        method: 'POST',
-        credentials: 'include',
-    }).then(() => disconnectedState());
+    await ssx?.signOut();
+    return disconnectedState();
 };
 
 /**
